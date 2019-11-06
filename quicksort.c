9a604438691e529ebca0a3c02cf94316b8e784cc
Shanky-21 <shashankdwivedi9648@gmail.com>
2019-11-06 16:22:38 +0530

    last element as pivot

---
+++ quicksort.c
@@ -0,0 +1,46 @@
+/*Quick_sort */
+#include <stdio.h>
+/*void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}*/
+int partition(int arr[],int left,int right){
+    int pivot,i,j,temp;
+    pivot = arr[right];
+    i = left-1;
+    j = left;
+    for(j = left;j<right;j++){
+        if(arr[j]<pivot){
+            i++;
+            temp = arr[j];
+            arr[j]= arr[i];
+            arr[i]= temp;
+        }
+    }
+    temp = arr[i+1];
+    arr[i+1]= arr[right];
+    arr[right] = temp;
+    return(i+1);
+}
+void quicksort(int arr[],int left,int right){
+    if(left<right){
+        int part = partition(arr,left,right);
+        quicksort(arr,left,part-1);
+        quicksort(arr,part+1,right);
+    }
+}
+
+int main(){
+    int i,n;
+    printf("Enter size of your array\n");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter elements of array:\n");
+    for(i = 0;i<n;i++)
+        scanf("%d",&arr[i]);
+    quicksort(arr,0,n-1);
+    printf("sorted array is:\n");
+    for(i = 0;i<n;i++)
+        printf("%d\t",arr[i]);
+}
